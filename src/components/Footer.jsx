e8abb45d76bf4ce873e420f24197c0bd24482ee1
elphus305 <makorielphus@gmail.com>
2026-03-30 13:58:53 +0300

    third commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Footer = () => {
   return (
-<section className="col-md-12">
-    <footer className='footer-container bg-warning'>
-      <div className='footer-content col-md-12'>
+<section className="col-md-12 ">
+    <div className="row">
+    <footer className='footer-container bg-warning '>
+      <div className='footer-content col'>
         <div className='footer-section'>
-          <div className='text-dark'>
+          
             <h4 className='text-center'>About Us</h4>
               <li><p>Welcome to ololua deals and hotels, where modern luxury meets heartfelt hospitality in the heart of Nairobi. Established to offer a serene escape, we provide elegantly designed rooms, personalized service, and exceptional amenities tailored for both leisure and business travelers. Experience the perfect blend of comfort and charm—your home away from home awaits. </p></li>
               <li>
@@ -15,34 +18,32 @@ const Footer = () => {
               </p></li>
           </div>
         </div>
-      </div>
+    
 
-      <div className="footer-content col-md-4">
-        <div className="footer-section">
-          <div className="footer-section">
-            <div className=" text-dark">
+      <div className="footer-content ">
+        <div className="footer-section col">
+         
+            
                 <h4>info</h4>
                 <li><p>Detailed Property Descriptions: Clear, honest information about room types, amenities (e.g., Wi-Fi, pool), and unique selling points.</p></li>
                 <li><p>High-Quality Visuals: Professional photos and videos that showcase the property and its surroundings.</p></li>
                 <li><p>Real-Time Booking Engine: Enables instant, secure reservations and showcases accurate, real-time availability and pricing.</p></li>
                 <li><p>Guest Reviews & Ratings: Transparent feedback to build trust and credibility.</p></li>
                 <li><p>Clear Location Information: Maps and details on proximity to attractions or transportation.</p></li>
-                <li><p>Contact Information: Easily accessible, responsive customer support options.</p></li>
-            </div>
+                <li><p>Contact Information: Easily accessible, responsive customer support options.</p></li>        
 
-          </div>
-        
-      </div>
-         </div>
-            <div className="col-md-12 footer-content">
-            <div className="footer-section">
-                <div className="text-dark">
+            </div> <div className=" footer-content ">
+            <div className="footer-section col">
                     <h4>For more info you can find us throgh our varios platforms</h4>
-                </div>
+                    <img src="images/instagram.jpeg" alt="" />
+                    <a href="http//:www.instgram.com">Instagram</a>
             </div>
         </div>
-    
+
+          </div>
     </footer>
+    
+    </div>
 </section>
   )
 }
